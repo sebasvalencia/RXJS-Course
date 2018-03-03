431ea06b6c4f3fd11ef14e15a2c3566ccc222d10
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-03-03 18:46:18 -0500

    rxjs course - create first component browser-event-experiments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BrowserEventExperimentsComponent } from './browser-event-experiments/browser-event-experiments.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserEventExperimentsComponent
   ],
   imports: [
     BrowserModule
