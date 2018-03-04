a511ca57c0f38630e2600c2f4eb93981df4730a6
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-03-03 19:03:52 -0500

    rxjs course - The way that use events

--- src/app/browser-event-experiments/browser-event-experiments.component.ts
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrowserEventExperimentsComponent implements OnInit {
 
+  hoverSection: HTMLElement;
+
   constructor() { }
 
   ngOnInit() {
+    this.hoverSection = document.getElementById('hover');
+
+    // We are receiveng a continuous stream of events, each event
+    // contains some data
+    this.hoverSection.addEventListener('mousemove', ev => {
+      console.log(ev); // print the event, corresponde to event -mouse coordinate
+    });
+
+
   }
 
 }
