ed0a2d39fba6d52b4897d1509dddd02afdf4c1ab
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-03-03 19:24:38 -0500

    rxjs course - Browser Events and reactive programming - import similarity

--- src/app/browser-event-experiments/browser-event-experiments.component.ts
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -9,18 +9,24 @@ export class BrowserEventExperimentsComponent implements OnInit {
 
   hoverSection: HTMLElement;
 
-  constructor() { }
-
   ngOnInit() {
     this.hoverSection = document.getElementById('hover');
 
     // We are receiveng a continuous stream of events, each event
     // contains some data
-    this.hoverSection.addEventListener('mousemove', ev => {
-      console.log(ev); // print the event, corresponde to event -mouse coordinate
-    });
-
+    this.hoverSection.addEventListener('mousemove', onMouseMove);
 
   }
 
+  unsuscribe() {
+    console.log('Called Unsuscribe()');
+    this.hoverSection.removeEventListener('mousemove', onMouseMove);
+  }
+
+}
+
+// The advantage is that we can actually now unsubscribe from this sequence
+// of events from this stream of mouse events.
+function onMouseMove(ev: MouseEvent) {
+  console.log(ev);
 }
